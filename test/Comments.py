a018db72baa6216c021ebd04c13bef484a1d8a4d
matigruca <matigruca@fdb21ef1-2011-0410-befe-b5e4ea1792b1>
2008-07-07 22:10:18 +0000

    Added tests for comments stripping framework
    git-svn-id: http://scons.tigris.org/svn/scons/branches/comments@3178 fdb21ef1-2011-0410-befe-b5e4ea1792b1

--- test/Comments.py
+++ test/Comments.py
@@ -5,6 +5,7 @@ import Comments
 # test C-stripper
 
 cfile = 'ccomments.c'
+cofile = 'ccomments.o'
 ccode = r"""
 #include <stdio.h>
 /* multiline
@@ -24,34 +25,80 @@ int main(void)
 }
 """
 
+# quote in first printf() changed
+ccode_quote_changed = r"""
+#include <stdio.h>
+/* multiline
+ * comment
+ */
+int main(void)
+{
+   int w = 10 + /* comment */ 1;
+   char x = '\'';
+   char y = '\n';
+   printf("CHANGE HERE\n\\\"\\");
+   printf("\"/\\/* not comment\"/\\/*\n"); // two-slashes-comment
+   printf("'///*not comment*/'"); // two-slashes-comment
+   printf("/"); /* comment */
+   printf("/**\"not comment*/");
+   return 0;
+}
+"""
+
+
+
+# changed 'int w = 10' into 'int w = 11'
+ccode_code_changed = r"""
+#include <stdio.h>
+/* multiline
+ * comment
+ */
+int main(void)
+{
+   int w = 11 + /* comment */ 1;
+   char x = '\'';
+   char y = '\n';
+   printf("not comment\n\\\"\\");
+   printf("\"/\\/* not comment\"/\\/*\n"); // two-slashes-comment
+   printf("'///*not comment*/'"); // two-slashes-comment
+   printf("/"); /* comment */
+   printf("/**\"not comment*/");
+   return 0;
+}
+"""
+
 
 test = TestSCons.TestSCons()
 
-test.subdir('C-one', 'C-two', 'D-one', 'D-two', 'py-one', 'py-two', 'fortran-one', 'fortran-two')
+subdirs = 'C-one', 'C-two', 'D-one', 'D-two', 'py-one', 'py-two', 'fortran-one', 'fortran-two', 'sig-check1'
 
-test.write(['C-one', cfile], ccode)
+#test.subdir('C-one', 'C-two', 'D-one', 'D-two', 'py-one', 'py-two', 'fortran-one', 'fortran-two', 'sigs-check1')
 
-test.write(['C-one', 'SConstruct'],
+test.subdir(*subdirs)
+
+test.write([subdirs[0], cfile], ccode)
+
+test.write([subdirs[0], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripCComments('ccomments.c'))
 """)
 
-test.run(chdir = 'C-one', arguments = '-q -Q', stdout = r"""#include<stdio.h>intmain(void){intw=10+1;charx='\'';chary='\n';printf("not comment\n\\\"\\");printf("\"/\\/* not comment\"/\\/*\n");printf("'///*not comment*/'");printf("/");printf("/**\"not comment*/");return0;}
+test.run(chdir = subdirs[0], arguments = '-q -Q', stdout = r"""#include<stdio.h>intmain(void){intw=10+1;charx='\'';chary='\n';printf("not comment\n\\\"\\");printf("\"/\\/* not comment\"/\\/*\n");printf("'///*not comment*/'");printf("/");printf("/**\"not comment*/");return0;}
 """)
 
 
 
-test.write(['C-two', cfile], ccode)
+test.write([subdirs[1], cfile], ccode)
 
-test.write(['C-two', 'SConstruct'],
+test.write([subdirs[1], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripCCode('ccomments.c'))
 """)
 
 
-test.run(chdir = 'C-two', arguments = '-q -Q', stdout = r"""/*multiline*comment*//*comment*///two-slashes-comment//two-slashes-comment/*comment*/
+test.run(chdir = subdirs[1], arguments = '-q -Q', stdout = r"""/*multiline*comment*//*comment*///two-slashes-comment//two-slashes-comment/*comment*/
 """)
 
 
@@ -76,28 +123,28 @@ void main()
 """
 
 
-test.write(['D-one', dfile], dcode)
+test.write([subdirs[2], dfile], dcode)
 
-test.write(['D-one', 'SConstruct'],
+test.write([subdirs[2], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripDComments('dcomments.d'))
 """)
 
-test.run(chdir = 'D-one', arguments = '-q -Q', stdout = r"""importstd.stdio;voidmain(){writefln("/+\" not \\\\\"comment\" +/");writefln("//\\\\\" not comment");writefln("/* not comment */");}
+test.run(chdir = subdirs[2], arguments = '-q -Q', stdout = r"""importstd.stdio;voidmain(){writefln("/+\" not \\\\\"comment\" +/");writefln("//\\\\\" not comment");writefln("/* not comment */");}
 """)
 
 
-test.write(['D-two', dfile], dcode)
+test.write([subdirs[3], dfile], dcode)
 
-test.write(['D-two', 'SConstruct'],
+test.write([subdirs[3], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripDCode('dcomments.d'))
 """)
 
 
-test.run(chdir = 'D-two', arguments = '-q -Q', stdout = r"""/+thisisanembeddedcomment/*thisisacommentwithinanembeddedcomment*/+////+comment+//*comment*//+comment+///comment
+test.run(chdir = subdirs[3], arguments = '-q -Q', stdout = r"""/+thisisanembeddedcomment/*thisisacommentwithinanembeddedcomment*/+////+comment+//*comment*//+comment+///comment
 """)
 
 
@@ -115,29 +162,29 @@ print "'#! not comment'" # comment
 # comment
 """
 
-test.write(['py-one', pyfile], pycode)
+test.write([subdirs[4], pyfile], pycode)
 
-test.write(['py-one', 'SConstruct'],
+test.write([subdirs[4], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripComments('pycomments.py'))
 """)
 
-test.run(chdir = 'py-one', arguments = '-q -Q', stdout = r"""print"#! not comment\\\\\""print'#! not comment'print'"#! not comment"'print"'#! not comment'"
+test.run(chdir = subdirs[4], arguments = '-q -Q', stdout = r"""print"#! not comment\\\\\""print'#! not comment'print'"#! not comment"'print"'#! not comment'"
 """)
 
 
 
-test.write(['py-two', pyfile], pycode)
+test.write([subdirs[5], pyfile], pycode)
 
-test.write(['py-two', 'SConstruct'],
+test.write([subdirs[5], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripCode('pycomments.py'))
 """)
 
 
-test.run(chdir = 'py-two', arguments = '-q -Q', stdout = r"""#!/usr/bin/envpython#comment#comment#comment#comment#comment#comment
+test.run(chdir = subdirs[5], arguments = '-q -Q', stdout = r"""#!/usr/bin/envpython#comment#comment#comment#comment#comment#comment
 """)
 
 
@@ -158,26 +205,72 @@ end program hello
 ! comment
 """
 
-test.write(['fortran-one', ffile], fcode)
+test.write([subdirs[6], ffile], fcode)
 
-test.write(['fortran-one', 'SConstruct'],
+test.write([subdirs[6], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripFortranComments('fcomments.f90'))
 """)
 
-test.run(chdir = 'fortran-one', arguments = '-q -Q', stdout = r"""programhelloprint*,"! not comment"print*,'! not comment'print*,'"! not comment"'print*,"'! not comment'"endprogramhello
+test.run(chdir = subdirs[6], arguments = '-q -Q', stdout = r"""programhelloprint*,"! not comment"print*,'! not comment'print*,'"! not comment"'print*,"'! not comment'"endprogramhello
 """)
 
 
-test.write(['fortran-two', ffile], fcode)
+test.write([subdirs[7], ffile], fcode)
 
-test.write(['fortran-two', 'SConstruct'],
+test.write([subdirs[7], 'SConstruct'],
       """
 import Comments
 print r"%s" % (Comments.StripFortranCode('fcomments.f90'))
 """)
 
 
-test.run(chdir = 'fortran-two', arguments = '-q -Q', stdout = r"""!comment!comment!comment!comment
+test.run(chdir = subdirs[7], arguments = '-q -Q', stdout = r"""!comment!comment!comment!comment
 """)
+
+
+### comments stripper framework tests
+
+test.write([subdirs[8], cfile], ccode)
+
+test.write([subdirs[8], 'SConstruct'],
+"""
+env = Environment()
+env.Program('%s')
+""" % cfile)
+
+# first compilation - should compile
+test.run(chdir = subdirs[8], arguments = '-Q', stdout = r"""gcc -o %s -c %s
+gcc -o ccomments %s
+""" % (cofile, cfile, cofile))
+
+# second try - up to date, so do not compile
+test.run(chdir = subdirs[8], arguments = '-Q', stdout = r"""scons: `.' is up to date.
+""")
+
+# comment added - should not rebuild
+test.write([subdirs[8], cfile], ccode + '// comment')
+
+test.run(chdir = subdirs[8], arguments = '-Q', stdout = r"""scons: `.' is up to date.
+""")
+
+# code changed - should rebuild
+test.write([subdirs[8], cfile], ccode_code_changed)
+
+test.run(chdir = subdirs[8], arguments = '-Q', stdout = r"""gcc -o %s -c %s
+gcc -o ccomments %s
+""" % (cofile, cfile, cofile))
+
+
+# quote in printf() changed - should rebuild
+test.write([subdirs[8], cfile], ccode_quote_changed)
+
+test.run(chdir = subdirs[8], arguments = '-Q', stdout = r"""gcc -o %s -c %s
+gcc -o ccomments %s
+""" % (cofile, cfile, cofile))
+
+# no changes - do not rebuild
+test.run(chdir = subdirs[8], arguments = '-Q', stdout = r"""scons: `.' is up to date.
+""")
+
